8316a8e2cadc62a042752397eb8b2edc1ca7816c
Sven James <sven@codehutsolutions.com>
2015-06-01 03:58:27 -0400

    Reconfigured Project to be WadAPI Hosted

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,9 @@ declare -r EXCLUDES=$(dirname $BASH_SOURCE)/exclude.txt
 declare -r REPO_ROOT=$(dirname $BASH_SOURCE)
 
 if [ "$1" = "dev" ]; then
-	cp wadadli/conf/settings.dev.xml wadadli/conf/settings.xml
 	declare -r TARGET_DIR=/var/www/html/chantico
 elif [ "$1" = "prod" ]; then
-	cp wadadli/conf/settings.prod.xml wadadli/conf/settings.xml
-	declare -r TARGET_DIR=datashanty@datashanty.com:public_html/gbsv/chantico
+	declare -r TARGET_DIR=login@mywadapi.com:api
 else
 	echo "Please specify one of [dev/prod] as deploy target"
 	exit
--- wadadli/bin/wadadli.php
+++
@@ -1,7 +0,0 @@
-<?php
-        require_once(dirname(__FILE__)."/../lib/includes.inc");
-        
-        //Dispatch Request
-        $activeDispatcher = SettingsManager::getSetting("install","dispatcher");
-        $activeDispatcher::dispatchRequest();
-?>
