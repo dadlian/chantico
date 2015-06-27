b44252e961eaeb553af625f1f8426cd31312c5b0
Sven James <sveninem@gmail.com>
2015-06-27 14:42:56 -0400

    Finalised Sandbox Configuration

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 declare -r EXCLUDES=$(dirname $BASH_SOURCE)/exclude.txt
 declare -r REPO_ROOT=$(dirname $BASH_SOURCE)
 
-if [ "$1" = "dev" ]; then
-	declare -r TARGET_DIR=/var/www/html/chantico
+if [ "$1" = "sandbox" ]; then
+	declare -r TARGET_DIR=loginsandbox@mywadapi.com:api
 elif [ "$1" = "prod" ]; then
 	declare -r TARGET_DIR=login@mywadapi.com:api
 else
@@ -13,6 +13,8 @@ else
 fi
 
 if [ "$2" = "go" ];then
+	#Copy Relevant Settings File
+	cp settings.$1.xml settings.xml
 	rsync -rltzuv --itemize-changes --delete -O --exclude-from $EXCLUDES $REPO_ROOT $TARGET_DIR
 else
 	rsync -rltzuv --itemize-changes --delete -O --dry-run --exclude-from $EXCLUDES $REPO_ROOT $TARGET_DIR
