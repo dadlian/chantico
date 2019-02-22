25af70d60163126b81e57a68aa4f3137c5b8b5a4
Sven James <sven@kycsar.com>
2019-02-21 23:08:21 -0400

    Made changes for Main User Endpoints

---
+++ migrate.php
@@ -0,0 +1,53 @@
+#!/usr/bin/php
+
+<?php
+  require 'vendor/autoload.php';
+  define('PROJECT_PATH', dirname(__FILE__));
+
+  use Wadapi\Persistence\DatabaseAdministrator;
+  use Wadapi\System\Janitor;
+
+  $apiId = "14915100262884";
+  DatabaseAdministrator::buildConnection("mywadapi.com","wadapi_sandbox","e=e%WGbqFkS3","wadapi_sandbox","chantico");
+
+  $resources = array();
+  $apiTokens = array();
+  $users = array();
+  $userOptions = array();
+
+  foreach(DatabaseAdministrator::execute("SELECT * FROM ManagedAccess WHERE api = '$apiId'") as $managedAccess){
+    $apiToken = DatabaseAdministrator::execute("SELECT * FROM APIToken WHERE accessKey = MD5('{$managedAccess['username']}')")[0];
+    $user = $managedAccess;
+
+    $resources[] = DatabaseAdministrator::execute("SELECT * FROM Resource WHERE id = {$apiToken['id']}")[0];
+    $resources[] = DatabaseAdministrator::execute("SELECT * FROM Resource WHERE id = {$user['id']}")[0];
+
+    $apiTokens[] = $apiToken;
+    $users[] = $user;
+
+    $userOptions[] = DatabaseAdministrator::execute("SELECT * FROM ManagedAccessOptions WHERE managedaccess = '{$user['id']}'");
+  }
+
+  DatabaseAdministrator::buildConnection("localhost","root","c0r1nTh14n","chantico","chantico");
+
+  foreach($resources as $resource){
+    DatabaseAdministrator::execute("INSERT INTO Resource VALUES('{$resource['id']}','{$resource['created']}','{$resource['modified']}')");
+  }
+
+  foreach($apiTokens as $apiToken){
+    DatabaseAdministrator::execute("INSERT INTO APIToken VALUES('{$apiToken['id']}','{$apiToken['created']}','{$apiToken['modified']}','{$apiToken['role']}','{$apiToken['expires']}','{$apiToken['accessKey']}','{$apiToken['accessSecret']}','{$apiToken['refreshSecret']}','".($apiToken['disabled']?$apiToken['disabled']:0)."',NULL)");
+  }
+
+  foreach($users as $user){
+    DatabaseAdministrator::execute("INSERT INTO ManagedAccess VALUES('{$user['id']}','{$user['created']}','{$user['modified']}','{$user['username']}','{$user['authentication']}','{$user['accessKey']}','{$user['accessSecret']}','{$user['refreshSecret']}','{$user['accessEndpoint']}','{$user['expires']}','{$user['multiSession']}')");
+  }
+
+  foreach($userOptions as $userOption){
+    foreach($userOption as $option){
+      $option['value'] = preg_replace("/'/","",$option['value']);
+      DatabaseAdministrator::execute("INSERT INTO ManagedAccessOptions VALUES('{$option['managedaccess']}','{$option['name']}','{$option['value']}')");
+    }
+  }
+
+  Janitor::cleanup();
+?>
